ceb392721f768bb818dac83f624369dd627a4ecd
cicero <cicerolz@gmail.com>
2014-04-15 22:50:43 -0300

    implementando sub / minus / mul / alguns faceis

--- src/llvmast/LlvmMinus.java
+++ src/llvmast/LlvmMinus.java
@@ -1,12 +1,18 @@
 package llvmast;
 
 public  class LlvmMinus extends LlvmInstruction{
-
+    public LlvmRegister _lhs;
+    public LlvmType _type;
+    public LlvmValue _op1, _op2;
+    
 	public LlvmMinus(LlvmRegister lhs, LlvmType type, LlvmValue op1, LlvmValue op2){
-
+		_lhs = lhs;
+		_op1 = op1;
+		_op2 = op2;
+		_type = type;
     }
 
     public String toString(){
-		return null;
+    	return "  " + _lhs + " = sub " + _type + " " + _op1 + ", " + _op2;
     }
 }
--- src/llvmast/LlvmTimes.java
+++ src/llvmast/LlvmTimes.java
@@ -1,10 +1,19 @@
 package llvmast;
 public  class LlvmTimes extends LlvmInstruction{
-
+	public LlvmRegister _lhs;
+	public LlvmType _type;
+	public LlvmValue _op1;
+	public LlvmValue _op2;
+	
+	
     public LlvmTimes(LlvmRegister lhs, LlvmType type, LlvmValue op1, LlvmValue op2){
+    	_lhs = lhs;
+    	_type = type;
+    	_op1 = op1;
+    	_op2 = op2;
     }
 
     public String toString(){
-		return null;
+		return " " + _lhs + " = mul " + _type + " " + _op1 + ", " + _op2;
     }
 }
