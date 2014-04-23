fa7b9f1bd26ee1fb071ebfecf57b80cdd6c7dcbd
cicero <cicerolz@gmail.com>
2014-04-22 22:00:32 -0300

    Criando declarador de classes

--- src/llvmast/LlvmClassDeclSimple.java
+++ src/llvmast/LlvmClassDeclSimple.java
@@ -1,15 +1,21 @@
 package llvmast;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 
 import syntaxtree.Identifier;
 import syntaxtree.MethodDecl;
 import syntaxtree.VarDecl;
+
 import util.List;
 
 public class LlvmClassDeclSimple extends LlvmInstruction{
 	public LlvmRegister _exp;
 	public LlvmType _type;
-	public util.List<MethodDecl> _methodList;
-	public util.List<VarDecl> _varList;
+	public List<MethodDecl> _methodList;
+	public List<VarDecl> _varList;
 	public Identifier _name;
 	
 	public LlvmClassDeclSimple(LlvmRegister exp, LlvmType type, Identifier name, List<VarDecl> varList,
@@ -17,19 +23,20 @@ public class LlvmClassDeclSimple extends LlvmInstruction{
 		_exp = exp;
 		_type = type;
 		_methodList = methodList;
-		_varList = varList;
+		_varList = varList.it
 		_name = name;	
 	}
 	
 	public String toString() {
-		String s = "%" + _name + " = type { " + _type + " }";
-		/*
-		for (int i = 0; i < _methodList.size(); i++) {
-			System.out.println(_methodList.get(i));
+		
+		
+		String variaveis = "";
+		for(VarDecl v : _varList){
+			variaveis += v.toString() + " ";
 		}
-		//s = s + 
-		return " " + _lhs + " = mul " + _type + " " + _op1 + ", " + _op2;
-		*/
+		
+		String s = "%" + _name + " = type { " + variaveis + " }";
+		
 		return s;
     }
 
