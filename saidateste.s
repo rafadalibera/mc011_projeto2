d976fd71607aea28614997d1761b76c33d6e8ca1
cicero <cicerolz@gmail.com>
2014-04-23 18:21:26 -0300

    Criando SymTab

---
+++ saidateste.s
@@ -0,0 +1,12 @@
+@.formatting.string = private constant [4 x i8] c"%d\0A\00"
+define i32 @main() {
+entry:
+  %tmp0 = alloca i32
+  store i32 0, i32 * %tmp0
+  %tmp1 = getelementptr [4 x i8] * @.formatting.string, i32 0, i32 0
+  %tmp2 = call i32 (i8 *, ...)* @printf(i8 * %tmp1, i32 10)
+  %tmp3 = load i32 * %tmp0
+  ret i32 %tmp3
+}
+declare i32 @printf (i8 *, ...)
+declare i8 * @malloc (i32)
