ec3f7ed6f66c92fd16ea6bed6118cdd6e919c34d
Rafael Dalibera <rafaeldalibera@gmail.com>
2014-05-03 01:54:15 -0300

    commit para pull

--- saidateste.s
+++ saidateste.s
@@ -3,37 +3,35 @@ define i32 @main() {
 entry:
   %tmp0 = alloca i32
   store i32 0, i32 * %tmp0
-  %tmp1 = call i8 *  @malloc(i32 8)
-  %tmp2 = bitcast i8 * %tmp1 to %class.good *
-  %tmp3 = call i32  @__func_good(%class.good * %tmp2)
+  %tmp1 = call i8 *  @malloc(i32 0)
+  %tmp2 = bitcast i8 * %tmp1 to %class.Fac *
+  %tmp3 = call i32  @__ComputeFac_Fac(%class.Fac * %tmp2, i32 10)
   %tmp4 = getelementptr [4 x i8] * @.formatting.string, i32 0, i32 0
   %tmp5 = call i32 (i8 *, ...)* @printf(i8 * %tmp4, i32 %tmp3)
   %tmp6 = load i32 * %tmp0
   ret i32 %tmp6
 }
-%class.good = type { i32, i32 }
-define i32 @__func_good(%class.good * %this) {
-  %tmp7 = getelementptr [4 x i8] * @.formatting.string, i32 0, i32 0
-  %tmp8 = call i32 (i8 *, ...)* @printf(i8 * %tmp7, i32 999)
-  ret i32 0
-}
-%class.good2 = type { }
-define i32 @__func_good2(%class.good2 * %this) {
-  ret i32 0
-}
-%class.k = type { i32, i32 }
-define i32 @__TestMethod_k(%class.k * %this) {
-  %tmp9 = alloca i32
-  %tmp10 = alloca %class.good *
-  %tmp11 = call i8 *  @malloc(i32 8)
-  %tmp12 = bitcast i8 * %tmp11 to %class.good *
-  store %class.good * %tmp12, %class.good * * %tmp10
-  %tmp13 = load %class.good * * %tmp10
-  %tmp14 = call i32  @__func_good(%class.good * %tmp13)
-  store i32 %tmp14, i32 * %tmp9
-  store i32 10, i32 * %tmp9
-  %tmp15 = getelementptr %class.k * %this, i32 0, i32 0
-  %tmp16 = load i32 * %tmp15
+%class.Fac = type { }
+define i32 @__ComputeFac_Fac(%class.Fac * %this, i32 %num) {
+  %tmp7 = alloca i32
+  store i32 %num, i32 * %tmp7
+  %tmp8 = alloca i32
+  %tmp9 = load i32 * %tmp7
+  %tmp10 = icmp slt i32 %tmp9, 1
+  br i1 %tmp10, label %label0, label %label1
+label0:
+  store i32 1, i32 * %tmp8
+  br label %label2
+label1:
+  %tmp11 = load i32 * %tmp7
+  %tmp12 = load i32 * %tmp7
+  %tmp13 = sub i32 %tmp12, 1
+  %tmp14 = call i32  @__ComputeFac_Fac(%class.Fac * %fac, i32 %tmp13)
+  %tmp15 = mul i32 %tmp11, %tmp14
+  store i32 %tmp15, i32 * %tmp8
+  br label %label2
+label2:
+  %tmp16 = load i32 * %tmp8
   ret i32 %tmp16
 }
 declare i32 @printf (i8 *, ...)
